1e7c43c3f940acb6feebabeb31c749e51dd75b3e
nopisos <77279082+nopisos@users.noreply.github.com>
2021-02-17 19:55:17 +0300

    grob

---
+++ Assets/Scripts/HpBarHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private Slider _slider;
+
+    private PlayerStatsHandler _stats;
+
+    private void Start()
+    {
+        _stats = _player.GetComponent<PlayerStatsHandler>();
+        _slider.maxValue = _stats._health;
+    }
+
+    public void SetSliderValue()
+    {
+        _slider.value = _stats._health;
+    }
+
+}
