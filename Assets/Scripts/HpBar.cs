e9a3c649fbf6c88f3ac7b16ce0d2875d80c374d5
nopisos <77279082+nopisos@users.noreply.github.com>
2021-02-24 17:45:49 +0300

    F

---
+++ Assets/Scripts/HpBar.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private Slider _slider;
+
+    private Player _stats;
+
+    private void Start()
+    {
+        _stats = _player.GetComponent<Player>();
+        _slider.maxValue = _stats._health;
+    }
+
+    public void SetSliderValue()
+    {
+        _slider.value = _stats._health;
+    }
+
+    //public void Heal()
+    //{
+    //    StartCoroutine(StartHeal());
+    //}
+
+    //private IEnumerator StartHeal()
+    //{
+    //    float targetValue = _stats._health;
+    //    float oneStepValue = _stats._oneStepValue;
+    //    while (_slider.value < targetValue && _slider.value != _slider.maxValue)
+    //    {
+    //        _slider.value += oneStepValue;
+    //        _text.text = _slider.value.ToString();
+    //        yield return new WaitForSeconds(_speed);
+    //    }
+    //}
+
+    //public void Damage()
+    //{
+    //    StartCoroutine(StartDamage());
+    //}
+
+    //private IEnumerator StartDamage()
+    //{
+    //    float targetValue = _stats._health;
+    //    float oneStepValue = _stats._oneStepValue;
+    //    while (_slider.value > targetValue && _slider.value != 0)
+    //    {
+    //        _slider.value -= oneStepValue;
+    //        _text.text = _slider.value.ToString();
+    //        yield return new WaitForSeconds(_speed);
+    //    }
+    //}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private float _speed;
+    [SerializeField] private UnityEvent SomeEvent;
+
+    public float _health;
+    public float _oneStepValue;
+    private float _maxHealth;
+
+    private void Start()
+    {
+        _maxHealth = _health;
+    }
+
+    public void Heal()
+    {
+        StartCoroutine(StartHeal());
+    }
+
+    private IEnumerator StartHeal()
+    {
+        float targetValue = _health + 10;
+        while (_health < targetValue && _health != _maxHealth)
+        {
+            _health += _oneStepValue;
+            SomeEvent.Invoke();
+            yield return new WaitForSeconds(_speed);
+        }
+    }
+
+    public void Damage()
+    {
+        StartCoroutine(StartDamage());
+    }
+
+    private IEnumerator StartDamage()
+    {
+        float targetValue = _health - 10;
+        while (_health > targetValue && _health != 0)
+        {
+            _health -= _oneStepValue;
+            SomeEvent.Invoke();
+            yield return new WaitForSeconds(_speed);
+        }
+    }
+}
