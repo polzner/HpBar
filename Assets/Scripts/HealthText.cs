eb955f62279d029a9872013ff6c8ad7030ac9846
nopisos <77279082+nopisos@users.noreply.github.com>
2021-03-01 00:05:59 +0300

    F

---
+++ Assets/Scripts/HealthText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthText : MonoBehaviour
+{
+    [SerializeField] private Text _text; 
+    [SerializeField] private Player _stats;
+
+    public void UpdateText()
+    {
+        _text.text = _stats._health.ToString();
+    }
+}
---
+++ Assets/Scripts/HpBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBar : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private Slider _slider;
+
+    private Player _stats;
+
+    private void Start()
+    {
+        _stats = _player.GetComponent<Player>();
+        _slider.maxValue = _stats._health;
+    }
+
+    public void SetSliderValue()
+    {
+        _slider.value = _stats._health;
+    }
+}
